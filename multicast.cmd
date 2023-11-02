2b18dc9895052e4a081a22ad63ba597084ed32ec
zak-45 <121941293+zak-45@users.noreply.github.com>
2023-11-02 14:24:18 +0100

    Add files via upload

--- multicast.cmd
+++ multicast.cmd
@@ -5,4 +5,4 @@ Rem will receive these parameters on entry " --ip " + myIP + " --group " + multi
 Rem which will give " --ip 127.0.0.1 --group 239.0.0.1 --port 19899" for example
 
 
-"%USERPROFILE%\Documents\Chataigne\Python\WPy64-39100\python-3.9.10.amd64\python.exe" "%USERPROFILE%\Documents\Chataigne\modules\WLEDAudioSync\multicast_msg.py" %1 %2 %3 %4 %5 %6
+"%USERPROFILE%\Documents\Chataigne\xtra\WLEDAudioSyncMCast-Windows.exe" %1 %2 %3 %4 %5 %6
